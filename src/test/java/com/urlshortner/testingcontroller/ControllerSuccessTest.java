698b91f3616e5c3329d243d5d88a4398a849fc82
ravikiranthota <ravikiranthota7@gmail.com>
2022-01-11 19:25:59 +0530

    testing controller layer

--- src/test/java/com/urlshortner/testingcontroller/ControllerSuccessTest.java
+++ src/test/java/com/urlshortner/testingcontroller/ControllerSuccessTest.java
@@ -1,40 +1,62 @@
 package com.urlshortner.testingcontroller;
 
 
+
+
+import com.urlshortner.JsonUtil;
 import com.urlshortner.controller.URLController;
 import com.urlshortner.dto.OriginalUrl;
 import com.urlshortner.dto.ShortUrl;
-
+import com.urlshortner.service.URLShortService;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(value = URLController.class)
 public class ControllerSuccessTest {
 
     @Autowired
-    private URLController urlController;
+    private MockMvc mvc;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    private int port=8080;
+    @MockBean
+    private URLShortService urlShortService;
 
     @Test
-    public void contextLoads() throws Exception{
-        assertThat(urlController).isNotNull();
+    public void when_createdShortUrl_checkForDuplicate_returnShortUrl() throws Exception {
+        ShortUrl shortUrl = new ShortUrl("http://localhost:8080/urlly/l");
+        given(urlShortService.createShortUrl(Mockito.any())).willReturn(shortUrl);
+
+        mvc.perform(post("/urlly/reduce").contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.toJson(shortUrl)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.shortUrl", CoreMatchers.is(shortUrl.getShortUrl())));
+
     }
 
     @Test
-    public void createShortURL(){
-        assertThat(this.restTemplate.postForObject("http://localhost:"+port+"/urly"+"/reduce",
-                new OriginalUrl("http://localhost:8080/urly/reduce"), ShortUrl.class).getShortUrl()).isEqualTo("l");
-    }
+    public void when_getOriginalUrl_returnOriginalUrl() throws Exception {
+        OriginalUrl originalUrl = new OriginalUrl("http://mail.google.com");
+        given(urlShortService.getFullUrl(Mockito.any())).willReturn(originalUrl);
 
+        mvc.perform(post("/urlly/retrieve").contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.toJson(originalUrl)))
+                .andExpect(jsonPath("$.originalUrl",CoreMatchers.is(originalUrl.getOriginalUrl())));
+    }
 
 }
